2915802ace411f02ade5ce9dea6b0994738ed0a6
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2017-11-21 12:09:49 +0100

    example

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,30 @@ import { __ } from 'ramda';
 import { createConnection } from 'typeorm';
 import { createAndAddTask, finishTask, Matrix } from './eisenhower/model/matrix.model';
 
+const matrix = Maybe.Just(1)
+    .map(Matrix)
+    .map(createAndAddTask(__, 'first task'))
+    .map(createAndAddTask(__, 'second task'))
+    .map(createAndAddTask(__, 'third task'))
+    .map(finishTask(__, 2))
+    .map((m) => ({
+        id: m.id,
+        tasks: m.tasks.toArray(),
+    }))
+    .just();
+
 createConnection()
     .then(async (connection) => {
-        const matrix = Maybe.Just(1)
-            .map(Matrix)
-            .map(createAndAddTask(__, 'first task'))
-            .map(createAndAddTask(__, 'second task'))
-            .map(createAndAddTask(__, 'third task'))
-            .map(finishTask(__, 2))
-            .map((m) => ({
-                id: m.id,
-                tasks: m.tasks.toArray(),
-            }))
-            .just();
+        const repository = connection.manager.getRepository('Matrix');
 
-        const result = await connection.manager.getRepository('Matrix').save(matrix);
+        const res0 = await repository.save(matrix);
 
-        console.log(result);
+        console.log('res0: ' + res0);
 
-        console.log('Matrix was saved');
+        const res1 = await  repository.findOneById(1);
+
+        console.log('res1: ' + res1);
+
+        connection.close();
     })
     .catch((error) => console.log(error));
