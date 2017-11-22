b0ab5ccdba8c186e3aba79ad0d6e9701eee0eaba
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2017-11-22 15:40:33 +0100

    Add missing types

--- src/common/infrastructure/repository/InMemoryRepository.ts
+++ src/common/infrastructure/repository/InMemoryRepository.ts
@@ -1,9 +1,9 @@
 import { Map } from 'immutable';
 import { Maybe } from 'monet';
-import { Identifiable } from '../../model/repository/repository';
+import { Identifiable, RepositoryGet, RepositoryStore } from '../../model/repository/repository';
 
-export const InMemoryGet = <ID, T extends Identifiable<ID>> (xs: Map<ID, T>) =>
+export const InMemoryGet = <ID, T extends Identifiable<ID>> (xs: Map<ID, T>): RepositoryGet<ID, T> =>
     (id: ID) => Maybe.fromNull(xs.get(id, undefined));
 
-export const InMemoryStore = <ID, T extends Identifiable<ID>> (xs: Map<ID, T>) =>
+export const InMemoryStore = <ID, T extends Identifiable<ID>> (xs: Map<ID, T>): RepositoryStore<ID, T> =>
     (x: T) => { xs.set(x.id, x); return Maybe.Just(x); };
